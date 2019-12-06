3b0e60938be7393db6332f6a90cada8048d96515
sudo-rushil <rushil.mallarapu@gmail.com>
2019-12-05 23:22:31 -0500

    merging changes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask, render_template, flash, redirect, url_for, request, sen
 from flask_wtf.csrf import validate_csrf
 from wtforms import ValidationError
 from forms import LoginForm
-from dgaintel import get_prediction
+from dgaintel import get_prob
 from intel_query import get_whois
 
 app = Flask(__name__)
@@ -20,7 +20,7 @@ def index():
     form = LoginForm()
     show_predictions_modal=False
     if form.validate_on_submit():
-        _prediction = get_prediction(form.domain.data)
+        _prediction = get_prob([form.domain.data])  
         form.whois = get_whois(form.domain.data)
         if _prediction == -1:
             form.prediction = "invalid (contains invalid characters)"
